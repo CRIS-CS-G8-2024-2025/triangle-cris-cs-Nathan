f11862fb3e786efa89fa48c27db28aa67f7e7602
N4thanL0L <nathan@cris.ac.th>
2024-12-06 10:11:45 +0700

    Nathan

---
+++ table.py
@@ -0,0 +1,6 @@
+"""
+Nathan, Computer Science, Nested Loops - Triangle assignment, Grade 8
+Making a python program that prints a 8 lines high and 8 characters width.
+"""
+for i in range(1, 8):
+    print("+ " * i)
